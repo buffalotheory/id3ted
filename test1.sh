1971a15eca581bc3a03f3f055e51ad2d90cd0380
Bryant Hansen <github.public@bryanthansen.net>
2014-03-29 01:43:18 +0100

    Added config file support (test.conf) Modified naming conventions

--- test1.sh
+++ test1.sh
@@ -1,50 +1,57 @@
 #!/bin/bash
 # Bryant Hansen
 
+# Description:
+# test script which writes the TBPM tag to a file, reads it back and verifies
+
 ME="$(basename "$0")"
 
-bin=./id3ted
-test_file_orig="/data/media/mp3/Vast/here.mp3"
+BIN=./id3ted
+TEST_FILE_ORIG=""
+TEST_DIR="."
+
+[[ test.conf ]] && [[ -f test.conf ]] && . test.conf
 
 # assume passed unless some stage fails
 test_result="passed"
 
-test_dir="."
-
 function TRACE() {
-	echo -e "${ME}: $1" >&2
+	echo -e "$(date "+%Y%m%d_%H%M%S") ${ME}: $1" >&2
 }
 
 TRACE "$(date): executing test to verify read/write of id3 BPM field"
 
-[[ "$1" ]] && test_file_orig="$1"
-file_under_test="$test_dir"/"$(basename "$test_file_orig")"
+[[ "$1" ]] && TEST_FILE_ORIG="$1"
+if [[ ! "$TEST_FILE_ORIG" ]] ; then
+	TRACE "ERROR: test audio file must be specified.  Exiting abnormally."
+	exit 2
+fi
 
-if $bin -l "$file_under_test" | grep "> [ \t]*TBPM:" > /dev/null ; then
+file_under_test="$TEST_DIR"/"$(basename "$TEST_FILE_ORIG")"
+
+TRACE "copying source testfile $TEST_FILE_ORIG to working file $file_under_test"
+cp -a "$TEST_FILE_ORIG" "$file_under_test"
+
+if $BIN -l "$file_under_test" | grep "> [ \t]*TBPM:" > /dev/null ; then
 	TRACE "TBPM already found in source audio file.  test failed."
 	test_result="failed"
 fi
 
-TRACE "copying source testfile $test_file_orig to working file $file_under_test"
-cp -a "$test_file_orig" "$file_under_test"
-
-tags_before="$($bin -l "$file_under_test")"
+tags_before="$($BIN -l "$file_under_test")"
 
 TRACE "writing TBPM to $file_under_test"
-$bin --TBPM "120.11" "$file_under_test"
+$BIN --TBPM "120.11" "$file_under_test"
 
-tags_after="$($bin -l "$file_under_test")"
+tags_after="$($BIN -l "$file_under_test")"
 
 res="$(diff <(echo -e "$tags_before") <(echo -e "$tags_after"))"
 
 TRACE "file modifications to ${file_under_test}:"
 echo -e "$res"
-
 if ! echo "$res" | grep "> [ \t]*TBPM:" > /dev/null ; then
 	TRACE "TBPM tag not added to file.  test failed."
 	test_result="failed"
 fi
-
 if [[ "$test_result" == "passed" ]] ; then
 	TRACE "test passed"
 else
