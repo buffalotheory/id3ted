f2740c4717fa2fe37e6b2f3ff162b3ecfd7047c4
Bryant Hansen <github.public@bryanthansen.net>
2014-03-29 03:07:17 +0100

    Parameterized TAG and VAL Check VAL for validity Added command-line params Added usage Added debug info

--- test1.sh
+++ test1.sh
@@ -6,9 +6,14 @@
 
 ME="$(basename "$0")"
 
+USAGE="${ME}  [ file  [  tag  [  value  ]  ]  ]"
+
 BIN=./id3ted
 TEST_FILE_ORIG=""
 TEST_DIR="."
+
+TAG="TBPM"
+VAL="120.11"
 
 [[ test.conf ]] && [[ -f test.conf ]] && . test.conf
 
@@ -27,20 +32,25 @@ if [[ ! "$TEST_FILE_ORIG" ]] ; then
 	exit 2
 fi
 
+[[ "$2" ]] && TAG="$2"
+[[ "$3" ]] && VAL="$3"
+
+TRACE "test params: BIN=$BIN  TEST_FILE=$TEST_FILE_ORIG  TAG=$TAG  VAL=$VAL"
+
 file_under_test="$TEST_DIR"/"$(basename "$TEST_FILE_ORIG")"
 
 TRACE "copying source testfile $TEST_FILE_ORIG to working file $file_under_test"
 cp -a "$TEST_FILE_ORIG" "$file_under_test"
 
-if $BIN -l "$file_under_test" | grep "> [ \t]*TBPM:" > /dev/null ; then
-	TRACE "TBPM already found in source audio file.  test failed."
+if $BIN -l "$file_under_test" | grep "> [ \t]*${TAG}:" > /dev/null ; then
+	TRACE "${TAG} already found in source audio file.  test failed."
 	test_result="failed"
 fi
 
 tags_before="$($BIN -l "$file_under_test")"
 
-TRACE "writing TBPM to $file_under_test"
-$BIN --TBPM "120.11" "$file_under_test"
+TRACE "writing ${TAG} to $file_under_test"
+$BIN --$TAG "$VAL" "$file_under_test"
 
 tags_after="$($BIN -l "$file_under_test")"
 
@@ -48,8 +58,8 @@ res="$(diff <(echo -e "$tags_before") <(echo -e "$tags_after"))"
 
 TRACE "file modifications to ${file_under_test}:"
 echo -e "$res"
-if ! echo "$res" | grep "> [ \t]*TBPM:" > /dev/null ; then
-	TRACE "TBPM tag not added to file.  test failed."
+if ! echo "$res" | grep "> [ \t]*${TAG}:[ \t]*${VAL}" > /dev/null ; then
+	TRACE "${TAG} tag not added to file.  test failed."
 	test_result="failed"
 fi
 if [[ "$test_result" == "passed" ]] ; then
